1ed64b4e8ce74c32745937775cfac02274624c59
NithikaaNivasanNS <nithikaa007@gmail.com>
2024-06-21 11:34:00 +0530

    initial commit

---
+++ src/main/java/com/nithikaa/student_management_system/service/StudentService.java
@@ -0,0 +1,19 @@
+package com.nithikaa.student_management_system.service;
+
+import com.nithikaa.student_management_system.entity.Student;
+
+import java.util.List;
+
+public interface StudentService {
+    List<Student> getAllStudents();
+
+    Student saveStudent(Student student);
+
+    Student getStudentById(Long id);
+    Student updateStudent(Student student);
+
+    void deleteStudentById(Long id);
+
+
+
+}
